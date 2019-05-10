f00a120bc4ea62a00649f44563df7ffd26098995
unknown <benjaminhancock@hotmail.co.uk>
2019-05-10 15:25:22 +0100

    first commit

---
+++ js.js
@@ -0,0 +1,20 @@
+// function hideMe() {
+//   var x = document.getElementById("myNotes");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
+
+
+<script>
+$(document).ready(function(){
+  $("#hide").click(function(){
+    $("p").hide();
+  });
+  $("#show").click(function(){
+    $("p").show();
+  });
+});
+</script>
